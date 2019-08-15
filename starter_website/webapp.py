a39dbdec09bac7516a43d270e6333d711d185e80
racestewart <35312415+racestewart@users.noreply.github.com>
2019-08-14 17:56:21 -0700

    updated webapp

--- starter_website/webapp.py
+++ starter_website/webapp.py
@@ -10,40 +10,72 @@ webapp = Flask(__name__)
 def hello():
     return "Hello World!";
 
-@webapp.route('/browse_bsg_people')
-#the name of this function is just a cosmetic thing
-def browse_people():
-    print("Fetching and rendering people web page")
+@webapp.route('/browse_games')
+def browse_games():
+    print("Fetching and rendering games")
     db_connection = connect_to_database()
-    query = "SELECT fname, lname, homeworld, age, character_id from bsg_people;"
+    query = "select * from Game;"
     result = execute_query(db_connection, query).fetchall();
     print(result)
-    return render_template('people_browse.html', rows=result)
+    return render_template('browse_games.html', rows=result)
 
-@webapp.route('/add_new_people', methods=['POST','GET'])
-def add_new_people():
+@webapp.route('/add_new_games', methods=['POST','GET'])
+def add_new_games():
     db_connection = connect_to_database()
-    if request.method == 'GET':
-        query = 'SELECT planet_id, name from bsg_planets'
+    if request.method == 'POST':
+        print("Add new games!");
+        name = request.form['Name']
+        date = request.form['Date']
+        price = request.form['Price']
+        query = "INSERT INTO Game (Name, Date, Price) VALUES (%s,%s,%s);"
+        data = (name, date, price)
+        execute_query(db_connection, query, data)
+        return ('Game added!');
+    else:
+        query = "SELECT ID, Name from Game;"
         result = execute_query(db_connection, query).fetchall();
         print(result)
+        return render_template('add_new_games.html', games = result)
 
-        return render_template('people_add_new.html', planets = result)
+
+@webapp.route('/update_games/<string:name>', methods=['POST','GET'])
+def update_games():
+    db_connection = connect_to_database()
+    #display existing data
+    if request.method == 'GET':
+        game_query = "SELECT id, name from Game WHERE name = %s" % (name)
+        game_result = execute_query(db_connection, game_query).fetchone()
+
+        if game_result == None:
+            return "No such game found!"
+
+        return render_template('update-game.html', game = game_result)
     elif request.method == 'POST':
-        print("Add new people!");
-        fname = request.form['fname']
-        lname = request.form['lname']
-        age = request.form['age']
-        homeworld = request.form['homeworld']
+        print("Update Game!");
+        name = request.form['name']
+        date = request.form['date']
+        price = request.form['price']
 
-        query = 'INSERT INTO bsg_people (fname, lname, age, homeworld) VALUES (%s,%s,%s,%s)'
-        data = (fname, lname, age, homeworld)
-        execute_query(db_connection, query, data)
-        return ('Person added!');
+        query = "UPDATE Game SET name = %s, date = %s, price = %s WHERE name = %s"
+        data = (name, date, price)
+        result = execute_query(db_connection, query, data)
+        print(str(result.rowcount) + " row(s) updated");
+
+        return redirect('/browse_games')
+
+@webapp.route('/delete_games/<int:id>')
+def delete_games(id):
+    '''deletes a game with the given id'''
+    db_connection = connect_to_database()
+    query = "DELETE FROM Game WHERE id = %s"
+    data = (id,)
+
+    result = execute_query(db_connection, query, data)
+    return (str(result.rowcount) + "row deleted")
 
 @webapp.route('/')
 def index():
-    return "<p>Are you looking for /db-test or /hello or <a href='/browse_bsg_people'>/browse_bsg_people</a> or /add_new_people or /update_people/id or /delete_people/id </p>"
+    return "<p>Are you looking for /browse_games or /hello or /add_new_games or /update_games/id or /delete_games/id </p>"
 
 @webapp.route('/db-test')
 def test_database_connection():
@@ -52,46 +84,3 @@ def test_database_connection():
     query = "SELECT * from bsg_people;"
     result = execute_query(db_connection, query);
     return render_template('db_test.html', rows=result)
-
-#display update form and process any updates, using the same function
-@webapp.route('/update_people/<int:id>', methods=['POST','GET'])
-def update_people(id):
-    db_connection = connect_to_database()
-    #display existing data
-    if request.method == 'GET':
-        people_query = 'SELECT character_id, fname, lname, homeworld, age from bsg_people WHERE character_id = %s' % (id)
-        people_result = execute_query(db_connection, people_query).fetchone()
-
-        if people_result == None:
-            return "No such person found!"
-
-        planets_query = 'SELECT planet_id, name from bsg_planets'
-        planets_results = execute_query(db_connection, planets_query).fetchall();
-
-        return render_template('people_update.html', planets = planets_results, person = people_result)
-    elif request.method == 'POST':
-        print("Update people!");
-        character_id = request.form['character_id']
-        fname = request.form['fname']
-        lname = request.form['lname']
-        age = request.form['age']
-        homeworld = request.form['homeworld']
-
-        print(request.form);
-
-        query = "UPDATE bsg_people SET fname = %s, lname = %s, age = %s, homeworld = %s WHERE character_id = %s"
-        data = (fname, lname, age, homeworld, character_id)
-        result = execute_query(db_connection, query, data)
-        print(str(result.rowcount) + " row(s) updated");
-
-        return redirect('/browse_bsg_people')
-
-@webapp.route('/delete_people/<int:id>')
-def delete_people(id):
-    '''deletes a person with the given id'''
-    db_connection = connect_to_database()
-    query = "DELETE FROM bsg_people WHERE character_id = %s"
-    data = (id,)
-
-    result = execute_query(db_connection, query, data)
-    return (str(result.rowcount) + "row deleted")
